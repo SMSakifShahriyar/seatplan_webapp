e48bdeaca156db3e8783bedd71ef81058f4d4dcb
Sakif <shakif1998@google.com>
2025-02-03 23:14:13 +0600

    Add Procfile and update app to use PORT environment variable

--- app.py
+++ app.py
@@ -111,5 +111,7 @@ def upload():
     
     return render_template('upload.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # Use the PORT environment variable if set, otherwise default to 8000
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
